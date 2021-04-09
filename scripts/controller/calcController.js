7698e8169920292ed3d74584cd364a403e30acab
pedrocarr <eng.pedro.amb@gmail.com>
2021-04-09 11:08:52 -0300

    method switch

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,8 @@
 class CalcController {
 
   constructor(){
+
+    this._operation = [];
     this._locale = 'pt-BR';
     this._displayCalcEl = document.querySelector("#display");
     this._dateEl = document.querySelector("#data");
@@ -23,6 +25,8 @@ class CalcController {
       }, 1000);
   }
 
+
+
   addEventListenerAll(element, events, fn){
 
       events.split(" ").forEach(event => {
@@ -31,6 +35,84 @@ class CalcController {
 
       });
 
+  }
+
+  clearAll(){
+
+    this._operation = [];
+
+  }
+
+  clearEntry(){
+
+    this._operation.pop();
+
+  }
+
+  addOperation(value){
+
+      this._operation.push(value);
+
+      console.log(this._operation);
+  }
+
+  setError(){
+
+    this.displayCalc = "Error";
+
+  }
+
+  execBtn(value){
+
+    switch(value){
+
+      case 'ac':
+        this.clearAll();
+        break;
+      case 'ce':
+        this.clearEntry();
+        break;
+      case 'soma':
+
+        break;
+      case 'soma':
+
+        break;
+      case 'subtracao':
+
+        break;
+      case 'divisao':
+
+        break;
+      case 'multiplicacao':
+
+        break;
+      case 'porcento':
+
+          break;
+      case 'igual':
+
+        break;
+
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+
+        this.addOperation(parseInt(value));
+        break;
+
+
+      default:
+        this.setError();
+        break;
+    }
   }
 
   initButtonsEvents(){
