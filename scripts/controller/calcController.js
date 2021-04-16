b87df97bdb0b94d65b588eafa77c32e5e732fb72
pedrocarr <eng.pedro.amb@gmail.com>
2021-04-16 07:57:58 -0300

    time to get this job done

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -44,29 +44,55 @@ class CalcController {
     return ["+", "-", "*", "%", "/"].indexOf(value) > -1;
   }
 
+  pushOperation(value) {
+
+    this._operation.push(value);
+
+    if (this._operation.length > 3) {
+
+        this.calc();
+
+    }
+  }
+
+  calc(){
+
+    let last = this._operation.pop();
+
+    let result = eval(this._operation.join(""));
+
+    this._operation = [result, last];
+
+  }
+  setLastNumberToDisplay(){
+
+  }
+
   addOperation(value) {
-    console.log("A", value, isNaN(this.getLastOperation()));
 
     if (isNaN(this.getLastOperation())) {
       if (this.isOperator(value)) {
         // Trocar o operador
-        this._setLastOperation(value);
+        this.setLastOperation(value);
       } else if (isNaN(value)) {
         // Outra coisa
-        console.log(value);
+        console.log("Outra coisa", value);
       } else {
-        this._operation.push(value);
+        this.pushOperation(value);
       }
     } else {
       if (this.isOperator(value)) {
-        this._operation.push(value);
+        this.pushOperation(value);
       } else {
         let newValue = this.getLastOperation().toString() + value.toString();
         this.setLastOperation(parseInt(newValue));
+
+        // atualizar display
+        this.setLastNumberToDisplay();
+
+        
       }
     }
-
-    console.log(this._operation);
   }
 
   setError() {
